2d5b55dbb243c76443f6446eb9b285dfd8f97607
Meepxzje <tmt.virut@gmail.com>
2025-03-13 14:32:40 +0700

    fixfe

--- TMT-Fe/vite.config.js
+++ TMT-Fe/vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
+
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    open: true,
+    watch: {
+      usePolling: true
+    },
+    allowedHosts: ["quanlynguoidung.onrender.com"]
+  }
+
 });
